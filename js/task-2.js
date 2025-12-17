3e2bbf5cde0289acda359d466e807ec39a127e82
TimothyYur <timjurev@gmail.com>
2025-12-17 21:18:04 +0200

    task 1,2 complete

--- js/task-2.js
+++ js/task-2.js
@@ -1,14 +1,41 @@
-function makeArray(firstArray, secondArray, maxLength){
-  let bigArr = firstArray.concat(secondArray)
-  let finalArr = bigArr
-  if (bigArr.length > maxLength){
-    finalArr = bigArr.slice(0, maxLength)
+function calcAverageCalories(days){
+  let caloriesArr = [];
+  let totalCalories = 0;
+  for(day of days){
+    caloriesArr.push(day.calories)
   }
-  return finalArr
+  for(value of caloriesArr){
+    totalCalories += value
+  }
+  if(caloriesArr.length === 0){
+    return totalCalories
+  }
+  return totalCalories / caloriesArr.length
 }
-console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
-console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
-console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
+console.log(
+  calcAverageCalories([
+    { day: "monday", calories: 3010 },
+    { day: "tuesday", calories: 3200 },
+    { day: "wednesday", calories: 3120 },
+    { day: "thursday", calories: 2900 },
+    { day: "friday", calories: 3450 },
+    { day: "saturday", calories: 3280 },
+    { day: "sunday", calories: 3300 }
+  ])
+); // 3180
+
+console.log(
+  calcAverageCalories([
+    { day: "monday", calories: 2040 },
+    { day: "tuesday", calories: 2270 },
+    { day: "wednesday", calories: 2420 },
+    { day: "thursday", calories: 1900 },
+    { day: "friday", calories: 2370 },
+    { day: "saturday", calories: 2280 },
+    { day: "sunday", calories: 2610 }
+  ])
+); // 2270
+
+console.log(
+  calcAverageCalories([])
+); // 0
