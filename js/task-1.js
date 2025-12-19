841f857aefbe06ff38e18c39f59bf380e7d596c8
TimothyYur <timjurev@gmail.com>
2025-12-19 20:59:17 +0200

    adding aditional test

--- js/task-1.js
+++ js/task-1.js
@@ -18,3 +18,7 @@ console.log(
 console.log(
   isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
 ); // true
+
+console.log(
+  isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+); // false
